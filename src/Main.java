8c7bd3d68dc9a730d92a5a8be918737dd52f1ec1
UmarGeeks <gumd0324@gmail.com>
2024-09-09 18:26:55 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("lkskmlsekfm!");
+        System.out.println(23456);
+
+        final String nameSurname = "gglrmdg";
+        final int NUM = 10;
+        int n;
+        double y = 3.5;
+        boolean g = true;
+        boolean w = false;
+
+        n = 37;
+
+        if(n < 20){
+            System.out.println("1");
+        }
+         else if (n > 0) {
+            System.out.println("2");
+        }
+        else {
+            System.out.println("3");
+        }
+        System.out.println(nameSurname);
+
+        String a = "Aman";
+        String b = "Ayana";
+
+        System.out.println(a + "+" + b);
+
+        Scanner scanner = new Scanner(System.in);
+        String sc = scanner.nextLine();
+        System.out.println("Hello " + sc);
+
+    }
+}
